fc080a1fb5f330d34a6f784d31949a6e19f31058
pmaxy <84446514+pmaxy@users.noreply.github.com>
2024-02-12 11:28:44 +0300

    change file path

--- E-HASIBU/src/components/User/Dashboard/sidebar/Sidebar.vue
+++ E-HASIBU/src/components/User/Dashboard/sidebar/Sidebar.vue
@@ -8,14 +8,14 @@
 
             <li  class="list_button  ">
               <router-link :to="{name:'Dashboard_home'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\house-solid.svg" class="w-10 bg-red-800 rounded-lg p-2 text-red " alt="dashboard">
+                <img src="@/assets/house-solid.svg" class="w-10 bg-red-800 rounded-lg p-2 text-red " alt="dashboard">
                 <span>Dashboard</span>
               </router-link>   
              
             </li>
             <li  class="list_button  ">
               <router-link :to="{name:'Sales'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Sales</span>
               </router-link>   
              
@@ -23,41 +23,41 @@
 
             <li  class="list_button  ">
               <router-link :to="{name:'Purchases'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Purchases</span>
               </router-link>   
              
             </li>    <li  class="list_button  ">
               <router-link :to="{name:'Inventory'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Inventory</span>
               </router-link>   
              
             </li>    
             <li  class="list_button  ">
               <router-link :to="{name:'Accounts'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Accounts</span>
               </router-link>   
              
             </li>    
             <li  class="list_button  ">
               <router-link :to="{name:'Budget'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Budget</span>
               </router-link>   
              
             </li>    
             <li  class="list_button  ">
               <router-link :to="{name:'Expenses'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Expenses</span>
               </router-link>   
              
             </li>    
             <li  class="list_button  ">
               <router-link :to="{name:'Reports'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Reports</span>
               </router-link> 
 
@@ -65,21 +65,21 @@
             </li> 
             <li  class="list_button  ">
               <router-link :to="{name:'Returns'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Returns</span>
               </router-link>   
              
             </li>  
              <li  class="list_button  ">
               <router-link :to="{name:'Settings'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Settings</span>
               </router-link>   
              
             </li>
             <li  class="list_button  ">
               <router-link :to="{name:'Feedback'}" class="p-2 bg-white rounded-lg  flex gap-2 items-center row hover:scale-[1.05]">
-                <img src="Z:\Projects\trial\EMTECH_E_HASIBU_FRONTEND\E-HASIBU\src\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
+                <img src="@\assets\internship-3833168-3185247.png" class="w-10 bg-red-800 rounded-lg p-2" alt="dashboard">
                 <span>Feedback</span>
               </router-link>   
              
