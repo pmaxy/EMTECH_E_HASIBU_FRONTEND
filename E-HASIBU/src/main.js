5d17f1c187ebb94f9aebc5254d1a1994a0c6b8af
Mutemi <josephmutemi2000@gmail.com>
2024-02-21 08:24:06 +0300

    added expenses module

--- E-HASIBU/src/main.js
+++ E-HASIBU/src/main.js
@@ -3,17 +3,15 @@ import './assets/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faHouse);
+
+
 
 
 const app = createApp(App)
 
 app.use(router)
-app.component('font-awesome-icon', FontAwesomeIcon)
+
 
 app.mount('#app')
